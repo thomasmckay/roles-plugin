3e1ab61b6033c945a9c406d1e09dcb9e548df677
Eric D. Helms <ericdhelms@gmail.com>
2014-09-30 14:12:40 +0100

    Convert to a Bastion based plugin.

--- app/controllers/roles/application_controller.rb
+++
@@ -1,6 +0,0 @@
-# TODO: is this file necessary?
-module Roles
-  # TODO: what should this be instead of Katello:: ?
-  class ApplicationController < Katello::ApplicationController
-  end
-end
--- roles.gemspec
+++ roles.gemspec
@@ -17,5 +17,6 @@ Gem::Specification.new do |s|
   s.test_files = Dir["test/**/*"]
 
   s.add_dependency "rails"
+  s.add_dependency "bastion"
 
 end
